b0612d94e6883688dd89a699232d893ba026e143
mfmendiola <mmendiol@usc.edu>
2012-10-31 15:05:47 -0700

    Testing

---
+++ Swing/src/PartsManPanel.java
@@ -0,0 +1,21 @@
+
+public class PartsManPanel extends JTabbedPane{
+
+	public PartsManPanel(){
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private class AddPanel extends JPanel {
+		
+		
+	}
+	
+	private class RemovePanel extends JPanel{
+		
+	}
+
+}
