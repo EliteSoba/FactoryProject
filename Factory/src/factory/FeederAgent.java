e1d33f965b993f98328bb66c2d95761c856ab331
Ryan Cleary <rcleary@usc.edu>
2012-11-01 19:43:39 -0700

    Added scheduler to the FeederAgent.

--- Factory/src/factory/FeederAgent.java
+++ Factory/src/factory/FeederAgent.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 
 import agent.Agent;
 import factory.interfaces.Feeder;
+import factory.interfaces.Gantry;
+import factory.interfaces.Lane;
+import factory.interfaces.Nest;
+import factory.interfaces.Vision;
 
 public class FeederAgent extends Agent implements Feeder {
 
 	/** DATA **/
-		   
 		   ArrayList<MyPartRequest> requestedParts = new ArrayList<MyPartRequest>();   
 		   MyLane topLane;
 		   MyLane bottomLane;
@@ -22,76 +25,144 @@ public class FeederAgent extends Agent implements Feeder {
 		   enum FeederState { EMPTY, WAITING_FOR_PARTS, CONTAINS_PARTS, OK_TO_PURGE, SHOULD_START_FEEDING }
 		   enum DiverterState { FEEDING_TOP, FEEDING_BOTTOM }
 
-		   class MyPartRequest {
 		      enum MyPartRequestState { NEEDED, ASKED_GANTRY, DELIVERED }
+		      
+		   class MyPartRequest {
 		      Part pt;
 		      MyPartRequestState state;
 		      Lane lane;
 
-		      public MyPart(PartType part, Lane lane){
-		            this.state = MyPartState.NEEDED;
+		      public MyPartRequest(Part part, Lane lane){
+		            this.state = MyPartRequestState.NEEDED;
 		            this.lane = lane;
 		            this.pt = part;
 		      }
 		   }
-
-		   class MyLane {
+		   
+		   
 		      enum MyLaneState {EMPTY, PURGING, CONTAINS_PARTS, BAD_NEST, 
 		      TOLD_NEST_TO_DUMP, NEST_SUCCESSFULLY_DUMPED}
 		      enum JamState {MIGHT_BE_JAMMED, TOLD_TO_INCREASE_AMPLITUDE, 
 		      AMPLITUDE_WAS_INCREASED, IS_NO_LONGER_JAMMED }
+		      
+		   class MyLane {
+
 		      Lane lane;
 		      MyLaneState state; 
 		      JamState jamState;
-		      Part
+		      Part part;
 
 		      public MyLane(Lane lane){
 		            this.state = MyLaneState.EMPTY;
 		            this.lane = lane;
 		      }
 		   }
+		   
 
 	/** MESSAGES **/
 		   public void msgEmptyNest(Nest n) {
-			   if (topLane.nest = n) 
+			   if (topLane.lane.getNest() == n) 
 			   {
-				   topLane.msgIncreaseAmplitude();
-				   topLane.jamState = MyLane.JamState.MIGHT_BE_JAMMED;
+				   topLane.lane.msgIncreaseAmplitude();
+				   topLane.jamState = JamState.MIGHT_BE_JAMMED;
 			   }
-			   else if (bottomLane.nest = n) 
+			   else if (bottomLane.lane.getNest() == n) 
 			   {
-				   bottomLane.msgIncreaseAmplitude();
-				   bottomLane.jamState = MyLane.JamState.MIGHT_BE_JAMMED;
+				   bottomLane.lane.msgIncreaseAmplitude();
+				   bottomLane.jamState = JamState.MIGHT_BE_JAMMED;
 			   }
 		   }
+
 		   public void msgNestWasDumped(Lane la) {
-			   if there exists MyLane l such that l.lane = la
-					   l.state = MyLane.MyLaneState.NEST_SUCCESSFULLY_DUMPED;
-			   end
+			   if (topLane.lane == la)
+			   {
+				   topLane.state = MyLaneState.NEST_SUCCESSFULLY_DUMPED;
+			   }
+			   else if (bottomLane.lane == la)
+			   {
+				   bottomLane.state = MyLaneState.NEST_SUCCESSFULLY_DUMPED;
+			   }
 		   }
+
 		   public void msgLaneNeedsPart(Part part, Lane lane) {
-			   myParts.add(new MyPartRequestState(part, lane));
+			   requestedParts.add(new MyPartRequest(part, lane));
 		   }
+
 		   public void msgHereAreParts(Part part) {
-			   for (requestedPart in requestedParts) {
-				   if(requestedPart.state == MyPartState.ASKED_GANTRY && requestedPart.pt == part) {
-					   requestedPart.state = MyPartState.DELIVERED;
+			   for (MyPartRequest pr : requestedParts) {
+				   if(pr.state == MyPartRequestState.ASKED_GANTRY && pr.pt == part) {
+					   pr.state = MyPartRequestState.DELIVERED;
 				   }
 			   }
 		   }
+
 		   public void msgBadNest(Nest n) {
-			   if (topLane.nest = n) 
-				   topLane.state = MyLane.MyLaneState.BAD_NEST;
-			   else if (bottomLane.nest = n)
-				   bottomLane.state = MyLane.MyLaneState.BAD_NEST;
+			   if (topLane.lane.getNest() == n) 
+				   topLane.state = MyLaneState.BAD_NEST;
+			   else if (bottomLane.lane.getNest() == n)
+				   bottomLane.state = MyLaneState.BAD_NEST;
 		   }
 
 		   
-	/** Scheduler **/
+	/** SCHEDULER **/
 	@Override
 	protected boolean pickAndExecuteAnAction() {
-		// TODO Auto-generated method stub
+
+		if (state == FeederState.EMPTY || state == FeederState.OK_TO_PURGE)
+		{
+			for (MyPartRequest p : requestedParts)
+			{
+				if (p.state == MyPartRequestState.NEEDED)
+				{
+				      askGantryForPart(p);
+				}
+			}
+		}
+		
+		for (MyPartRequest p : requestedParts) 
+		{
+			if (p.state == MyPartRequestState.DELIVERED)
+			{
+				processFeederParts();
+			}
+		}
+		
+		
+		
+		if (state == FeederState.SHOULD_START_FEEDING) 
+		{ 
+			// Which lane should it feed to?
+			if (topLane.part == currentPart && topLane.state == MyLaneState.EMPTY)
+			{
+				StartFeeding();
+			}
+			else if (bottomLane.part == currentPart && bottomLame.state = MyLaneState.EMPTY)
+			{
+				StartFeeding();
+			}
+		}
+
+		if (topLane.state == MyLaneState.BAD_NEST) 
+			dumpNest(topLane);
+
+		if (bottomLane.state == MyLaneState.BAD_NEST)
+			dumpNest(bottomLane);
+
+		if (topLane.state == MyLaneState.NEST_SUCCESSFULLY_DUMPED)
+			backToNormalAfterNestDump(topLane);
+
+		if (bottomLane.state == MyLaneState.NEST_SUCCESSFULLY_DUMPED)
+			backToNormalAfterNestDump(bottomLane);
+		
 		return false;
 	}
 
+	
 }
+
+
+
+
+
+
+
--- Factory/src/factory/interfaces/Lane.java
+++ Factory/src/factory/interfaces/Lane.java
@@ -2,4 +2,8 @@ package factory.interfaces;
 
 public interface Lane {
 
+public Nest getNest(); //returns this lane's nest
+
+public void msgIncreaseAmplitude();
+
 }
