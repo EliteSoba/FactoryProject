e8ee8db7c3f8031ccfe8d8c11760420c8016065e
dbay1991 <dbay1991@gmail.com>
2012-11-01 20:42:36 -0700

    Committing work for Ryan

--- Factory/src/factory/GantryAgent.java
+++ Factory/src/factory/GantryAgent.java
@@ -6,5 +6,56 @@ import java.util.*;
 import factory.interfaces.*;
 
 public class GantryAgent {
-	
+	   ArrayList<MyBin> myBins = new ArrayList<MyBin>();   
+	   BinConfig binConfig;
+	   
+	   enum MyBinState { NEEDED, DELIVERED}
+
+	   class MyBin {
+	      
+	      MyBinState state;
+	      Part pt;
+	      Feeder fdr;
+
+	      public MyBin(Part part, Feeder feeder){
+	            this.state = MyBinState.NEEDED;
+	            this.fdr = feeder;
+	            this.pt = part;
+	      }
+	   }
+	   
+	   
+	   
+	   
+	// *** MESSAGES ***
+	   
+	   public void msgFeederNeedsPart(Part part, Feeder feeder) {
+		   myBins.add(new MyBin(part, feeder));
+		}
+
+		public void msgChangeGantryBinConfig(BinConfig binConfig) {
+		   this.binConfig = binConfig;
+		}
+	   
+	   
+	// *** SCHEDULER ***
+	   
+	   public boolean pickAndExecuteAnAction() {
+
+		   for(MyBin b: myBins){
+			   if(b.state == MyBinState.NEEDED){
+				   goFetchTheRequestedBin(b);
+			   }
+		   }
+			return false;
+	   }
+	   
+	// *** ACTIONS ***
+	   private void goFetchTheRequestedBin(MyBin b) {
+		   DoPickupPurgeBin(); //animation message
+		   DoRefillPurgeBin(binConfig.get(b.pt));
+		   DoBringRequestedBin(binConfig.get(b.pt, b.fdr));
+		   b.fdr.msgHereIsPart(b.pt);
+		   b.state = MyBinState.DELIVERED;
+		}
 }
--- Factory/src/factory/interfaces/Feeder.java
+++ Factory/src/factory/interfaces/Feeder.java
@@ -1,5 +1,9 @@
 package factory.interfaces;
 
+import factory.Part;
+
 public interface Feeder {
+
+	void msgHereIsPart(Part pt);
 
 }
