cbbb9a8fed2e0ec3ff1ab10b544d5ce1ec9fd945
Ryan Cleary <rcleary@usc.edu>
2012-11-01 19:14:11 -0700

    Added LaneAgent, Lane interface, NestAgent, Nest interface, and Part.

---
+++ Factory/src/factory/Part.java
@@ -0,0 +1,7 @@
+package factory;
+
+public class Part {
+	String name;               // name of the part type
+	double averageDelayTime;   // The average time that the part takes to reach the nest to be used by the feeder
+	double productionTime;       // The time it takes to produce a piece when refilling
+}
