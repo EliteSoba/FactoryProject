ad64940156732142e10dddf1c1ff6c187712c48f
Austin Puri <austin.puri@gmail.com>
2012-11-01 18:40:24 -0700

    Added file structures

---
+++ Factory/src/factory/KitRobotAgent.java
@@ -0,0 +1,18 @@
+package factory;
+
+import factory.interfaces.*;
+import agent.Agent;
+
+public class KitRobotAgent extends Agent implements KitRobot {
+	////Data
+
+	////Messages
+	
+	////Scheduler
+	protected boolean pickAndExecuteAnAction() {
+		return false;
+	}
+	
+	////Actions
+
+}
---
+++ Factory/src/factory/test/mock/MockAgent.java
@@ -0,0 +1,19 @@
+
+package factory.test.mock;
+
+public class MockAgent {
+	private String name;
+
+	public MockAgent(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String toString() {
+		return this.getClass().getName() + ": " + name;
+	}
+
+}
