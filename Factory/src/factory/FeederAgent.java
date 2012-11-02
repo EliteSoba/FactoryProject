bc42f61f30ad22912a54de9b326e9c040587042f
Ryan Cleary <rcleary@usc.edu>
2012-11-01 18:55:34 -0700

    Added feederagent and feeder interface

---
+++ Factory/src/factory/FeederAgent.java
@@ -0,0 +1,16 @@
+package factory;
+
+import agent.Agent;
+import factory.interfaces.Feeder;
+
+public class FeederAgent extends Agent implements Feeder {
+
+	
+	/** Scheduler **/
+	@Override
+	protected boolean pickAndExecuteAnAction() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
