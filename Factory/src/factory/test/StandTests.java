660185ff8037ecc61823749af49aea303d737aa1
garzaa <alfonsogarza@me.com>
2012-11-03 01:01:36 -0700

    Added Stand Agent test file.

---
+++ Factory/src/factory/test/StandTests.java
@@ -0,0 +1,49 @@
+package factory.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class StandTests {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testStandAgent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMsgEmptyKitIsHere() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMsgHereIsAnEmptyKit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMsgKitRobotNoLongerUsingStand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMsgPartRobotWantsToPlaceParts() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMsgPartsRobotNoLongerUsingStand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMsgResultsOfKitAtInspection() {
+		fail("Not yet implemented");
+	}
+
+}
