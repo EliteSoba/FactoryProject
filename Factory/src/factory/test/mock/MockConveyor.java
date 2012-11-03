f0d6d08a278d1ad94055e0e0be26ae884c2210ff
garzaa <alfonsogarza@me.com>
2012-11-03 01:08:50 -0700

    Added a mock for Conveyor Agent.

---
+++ Factory/src/factory/test/mock/MockConveyor.java
@@ -0,0 +1,12 @@
+package factory.test.mock;
+
+import factory.interfaces.Conveyor;
+
+public class MockConveyor extends MockAgent implements Conveyor {
+
+	public MockConveyor(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+}
