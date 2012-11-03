e089f0629ff500639168568660f9f04d7e331747
garzaa <alfonsogarza@me.com>
2012-11-03 01:09:18 -0700

    Added a mock for PartsRobot Agent.

---
+++ Factory/src/factory/test/mock/MockPartsRobotAgent.java
@@ -0,0 +1,24 @@
+package factory.test.mock;
+
+import factory.interfaces.PartsRobot;
+
+public class MockPartsRobotAgent extends MockAgent implements PartsRobot {
+
+	public MockPartsRobotAgent(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void msgBuildKitAtSlot(String slot) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void msgDeliverKitParts() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
