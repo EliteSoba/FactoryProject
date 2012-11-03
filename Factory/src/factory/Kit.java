b4699ccb41fe2a563c550fd8a7ab5820dc205e9e
garzaa <alfonsogarza@me.com>
2012-11-02 17:50:15 -0700

    Added the shared data class Kit.

---
+++ Factory/src/factory/Kit.java
@@ -0,0 +1,15 @@
+package factory;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List; 
+
+import factory.Part;
+
+public class Kit {
+   enum KitState { INCOMPLETE, COMPLETE, PASSED_INSPECTION, FAILED_INSPECTION }
+   public KitState state;
+   public List<Part> parts = Collections.synchronizedList(new ArrayList<Part>());
+
+}
