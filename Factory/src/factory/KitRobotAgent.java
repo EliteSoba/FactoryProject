596df028b8a322d3b670f0fb26ba71d227bef374
garzaa <alfonsogarza@me.com>
2012-11-04 15:10:27 -0800

    Implemented methods from the instance in KitRobotAgent.

--- Factory/src/factory/KitRobotAgent.java
+++ Factory/src/factory/KitRobotAgent.java
@@ -4,17 +4,39 @@ import factory.interfaces.*;
 import agent.Agent;
 
 public class KitRobotAgent extends Agent implements KitRobot {
-	////Data
 
-	////Messages
+	/** DATA **/
 	
-	////Scheduler
+	/** MESSAGES **/
+	@Override
+	public void msgGrabAndBringEmptyKitFromConveyor() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgComeMoveKitToInspectionSlot(String slot) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgComeProcessAnalyzedKitAtInspectionSlot() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+	/** SCHEDULER **/
 	protected boolean pickAndExecuteAnAction() {
 		
 		return false;
 	}
 	
-	////Actions
+	/** ACTIONS **/
+
+	
+	/** ANIMATIONS **/
 
 }
 
