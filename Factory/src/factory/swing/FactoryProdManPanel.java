1be009e623aa74a2da0fee1e1204aa5723235670
Joey Huang <joeyhuan@usc.edu>
2012-11-04 15:22:50 -0800

    factory production manager w/o tester

--- Factory/src/factory/swing/FactoryProdManPanel.java
+++ Factory/src/factory/swing/FactoryProdManPanel.java
@@ -1,7 +1,5 @@
-// Assume factory production manager has arraylist<string> of kitnames that is used to initialize tihs class
+// Assume factory production manager has arraylist<string> kitList that is used to initialize tihs class
 
-/*import java.io.*;
-import java.net.*;*/
 package factory.swing;
 import java.util.*;
 import java.awt.*;
@@ -15,7 +13,7 @@ public class FactoryProdManPanel extends JPanel implements ActionListener {
 	JButton submitButton;
 	JTextArea messageBox;
 	
-	public FactoryProdManPanel(ArrayList<String> kitList) { // manager has arraylist of kitnames available
+	public FactoryProdManPanel() { // manager has arraylist of kitnames available
 		kitNameBox = new JComboBox();
 		for (int i = 0; i < kitList.size(); i++)
 			kitNameBox.addItem(kitList.get(i));
@@ -23,7 +21,6 @@ public class FactoryProdManPanel extends JPanel implements ActionListener {
 		
 		setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-	//	c.anchor = GridBagConstraints.PAGE_START;
 		c.gridy = 1;
 		c.gridx = 1;
 		c.gridwidth = 4;
@@ -70,55 +67,7 @@ public class FactoryProdManPanel extends JPanel implements ActionListener {
 		add(new JScrollPane(messageBox),c);
 				
 	}
-	
-	public FactoryProdManPanel() { 
-		kitNameBox = new JComboBox();
-		kitNameBox.setPreferredSize(new Dimension(225,25));
 		
-		setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		c.gridy = 1;
-		c.gridx = 1;
-		c.gridwidth = 4;
-		c.gridheight = 1;
-		c.insets = new Insets(0,0,25,0); 
-		add(new JLabel("Factory Production Manager"),c);
-		
-		
-		c.insets = new Insets(10,0,10,0);
-		c.gridy = 2;
-		c.anchor = GridBagConstraints.LINE_START;
-		add(new JLabel("Submit New Batch Order:"),c);
-		
-		c.gridy = 3;
-		c.anchor = GridBagConstraints.CENTER;
-		add(kitNameBox,c);
-
-		c.anchor = GridBagConstraints.LINE_END;
-		c.gridy = 4;
-		c.gridx = 2;
-		c.gridwidth = 1;
-		add(new JLabel("Quantity"),c);
-		
-		c.gridx = 4;
-		SpinnerNumberModel qntyModel = new SpinnerNumberModel(1,1,500,1);
-		spinner = new JSpinner(qntyModel);
-		add(spinner,c);
-		
-		c.gridy = 5;
-		c.gridx = 4;
-		submitButton = new JButton("Submit");
-		submitButton.addActionListener(this);
-		add(submitButton,c);
-		
-		c.gridy = 6;
-		c.gridx = 1;
-		c.gridwidth = 4;
-		messageBox = new JTextArea("System Messages...\n",10,20);
-		add(new JScrollPane(messageBox),c);
-				
-	}
-	
 	public void actionPerformed(ActionEvent ae) {
 		
 		if (ae.getSource().equals(submitButton)) {
@@ -129,7 +78,6 @@ public class FactoryProdManPanel extends JPanel implements ActionListener {
 		}
 	}
 	
-
 	public void addKit(String kitName) {	
 		kitNameBox.addItem(kitName);	
 	}
