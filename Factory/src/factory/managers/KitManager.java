e6f4721ce6ec54ac744cff404f9f086647fe9e5c
Ben Mayeux <benmayeux@gmail.com>
2012-11-04 15:39:20 -0800

    Added KitManager class

---
+++ Factory/src/factory/managers/KitManager.java
@@ -0,0 +1,27 @@
+package factory.managers;
+
+import javax.swing.JPanel;
+
+import factory.client.Client;
+import factory.swing.KitManPanel;
+
+public class KitManager extends Client {
+
+	public KitManager(JPanel buttons) {
+		super(Client.Type.KITMANAGER, buttons);
+		setInterface();
+	}
+	public static void main(String[] args){
+	    KitManPanel buttons = new KitManPanel();
+		KitManager k = new KitManager(buttons);
+	}
+
+	@Override
+	public void setInterface() {
+		this.setSize(800, 800);
+		this.add(UI);
+		this.setVisible(true);
+		
+	}
+
+}
