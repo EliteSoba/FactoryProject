71f085827f3570d847d83be51d83028f7652aec4
Ben Mayeux <benmayeux@gmail.com>
2012-11-04 16:11:06 -0800

    Added GantryManager

---
+++ Factory/src/factory/managers/GantryManager.java
@@ -0,0 +1,26 @@
+package factory.managers;
+
+import javax.swing.JPanel;
+
+import factory.client.Client;
+import factory.swing.GantryManPanel;
+
+public class GantryManager extends Client {
+
+	public GantryManager(JPanel buttons) {
+		super(Client.Type.GANTRYROBOTMANAGER, buttons, null);
+		setInterface();
+	}
+	public static void main(String[] args){
+	    GantryManPanel buttons = new GantryManPanel();
+		KitManager k = new KitManager(buttons);
+	}
+
+	@Override
+	public void setInterface() {
+		this.setSize(800, 800);
+		this.add(UI);
+		this.setVisible(true);
+		
+	}
+}
