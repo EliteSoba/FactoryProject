b561ab22711987c524521368f2931d58027e0b83
garzaa <alfonsogarza@me.com>
2012-11-04 18:33:41 -0800

    Deleted unused method.

--- Factory/src/factory/interfaces/KitRobot.java
+++ Factory/src/factory/interfaces/KitRobot.java
@@ -5,5 +5,4 @@ public interface KitRobot {
 	public void msgGrabAndBringEmptyKitFromConveyorToSlot(String slot);
 	public void msgComeMoveKitToInspectionSlot(String slot);
 	public void msgComeProcessAnalyzedKitAtInspectionSlot();
-	public void msgInspectionResults(InspectionResults inspectionResults); //can't pass enum, change to string?
 }
