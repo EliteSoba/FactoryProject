43629dd4b1a09a40231e892a27255461fd6d69cb
smreagle <smreagle@comcast.net>
2012-11-08 09:58:08 -0800

    made changes to LanManPanel based on design doc feedback

--- Factory/src/factory/swing/LaneManPanel.java
+++ Factory/src/factory/swing/LaneManPanel.java
@@ -5,41 +5,47 @@ package factory.swing;
 import java.awt.*;
 import javax.swing.*;
 
-public class LaneManPanel extends JPanel {
+public class LaneManPanel extends JFrame {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -4485912622490446254L;
-	JLabel lane1 = new JLabel("Lane 1");
+	String [] stringLanes = { "1", "2", "3", "4", "5", "6", "7", "8"};
+	JComboBox lane = new JComboBox(stringLanes);
+	//ImageIcon red = new ImageIcon("red.png");
+	JButton redButton = new JButton("Red");
+	JButton yellowButton = new JButton("Yellow");
+	JButton greenButton = new JButton("Green");
+	JButton powerButton = new JButton("Power");
 
 	public LaneManPanel(){
 		
 	    GridBagConstraints c = new GridBagConstraints();
 		setLayout(new GridBagLayout());
+		
 		c.fill = GridBagConstraints.VERTICAL;
 		c.gridx = 0;
 		c.gridy = 0;
-		add(zombie, c);
-
-		c.fill = GridBagConstraints.VERTICAL;
-		c.gridx = 0;
+		add(lane, c);
+		
+		c.gridx = 2;
+		add(redButton, c);
+		
 		c.gridy = 1;
-		add(robotMutiny, c);
-
-		c.fill = GridBagConstraints.VERTICAL;
-		c.gridx = 1;
+		add(yellowButton, c);
+		
+		c.gridy = 2; 
+		add(greenButton, c);
+		
+		c.gridx = 5;
 		c.gridy = 0;
-		add(fire, c);
+		add(powerButton);
 
-		c.fill = GridBagConstraints.VERTICAL;
-		c.gridx = 1;
-		c.gridy = 1;
-		add(earthquake, c);
 	
 	}
 	 //main method used for testing
 	//do not delete just comment out
-	/*
+	
 	public static void main (String[] args){
 		LaneManPanel l = new LaneManPanel();
 		l.repaint();
@@ -47,6 +53,6 @@ public class LaneManPanel extends JPanel {
 		l.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		l.setSize(400,450);
 		l.repaint(); 
-	}*/
+	}
 		
 }
