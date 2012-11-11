ea050c07e179bfb9f0e9647f4a78dc53066d5002
George Li <ligeorge@usc.edu>
2012-11-11 15:28:43 -0800

    Updated animatedobject

--- Factory/src/factory/graphics/AnimatedObject.java
+++ Factory/src/factory/graphics/AnimatedObject.java
@@ -1,7 +1,7 @@
 package factory.graphics;
-
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.util.ArrayList;
 
 class AnimatedObject
 {
@@ -11,6 +11,7 @@ class AnimatedObject
 	protected int dtheta;						// change in image angle
 	protected int imageWidth, imageHeight;		// image size
 	protected Image image;
+	protected ArrayList<GraphicItem> items;			// inventory of items
 	
 	// Constructors
 	public AnimatedObject()
@@ -75,7 +76,35 @@ class AnimatedObject
 	{
 		return imageHeight;
 	}
-	
+	public void addItem(GraphicItem newItem)
+	{
+		items.add(newItem);
+	}
+	public void clearItems()
+	{
+		items.clear();
+	}
+	public boolean hasItem()
+	{
+		if(items.size() >= 1)
+			return true;
+		else
+			return false;
+	}
+	public GraphicItem popItem()
+	{
+		GraphicItem lastItem = items.get(items.size()-1);		// get last item
+		items.remove(items.size()-1);					// remove last item
+		return lastItem;								// return last item
+	}
+	public int getSize()
+	{
+		return items.size();
+	}
+	public GraphicItem getItemAt(int i)
+	{
+		return items.get(i);
+	}
 	// Update functions
 	public void move()
 	{
