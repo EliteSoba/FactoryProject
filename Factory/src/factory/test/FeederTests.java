2920bf5c58164363daa2a81e13700b705aeba75a
Ryan Cleary <rcleary@usc.edu>
2012-11-13 17:27:28 -0800

    Trying to fix a bug in FeederAgent that is causing an incorrect lane switch / purge combination.

--- Factory/src/factory/test/FeederTests.java
+++ Factory/src/factory/test/FeederTests.java
@@ -1,5 +1,5 @@
 package factory.test;
-/*
+
 import factory.FeederAgent;
 import factory.FeederAgent.DiverterState;
 import factory.FeederAgent.FeederState;
@@ -9,6 +9,8 @@ import factory.FeederAgent.MyPartRequestState;
 import factory.Part;
 import factory.interfaces.Lane;
 import factory.interfaces.Nest;
+import factory.test.mock.EventLog;
+import factory.test.mock.LoggedEvent;
 import factory.test.mock.MockGantry;
 import factory.test.mock.MockLane;
 import factory.test.mock.MockNest;
@@ -20,40 +22,44 @@ public class FeederTests extends TestCase{
 	FeederAgent feeder;
 	MockLane top, bottom;
 	MockGantry gantry;
-	Part p1;
+	Part p1,p2;
+	
 	/**
 	 * Sets up the standard configuration for easy testing.
-	 
+	 */
 	@Override
 	protected void setUp() throws Exception {
-		feeder = new FeederAgent("Feeder1",0);
-		
 		top = new MockLane("top");
 		bottom = new MockLane("bottom");
 		gantry = new MockGantry("gantry");
-		p1 = new Part("p1");
 		
-		feeder.setGantry(gantry);
-		feeder.setUpLanes(top, bottom);
+		feeder = new FeederAgent("Feeder",0, top, bottom, gantry, null);
+		feeder.diverter = DiverterState.FEEDING_BOTTOM; // initial setting to test the switching of the lane diverter
+
+		
+		p1 = new Part("p1");
+		p2 = new Part("p2");
+		
 	}
 	
 	/**
 	 *  This test creates a feeder and tests its preconditions.
 	 * 
-	 
+	 */
 	public void testPreconditions() {
 		// Makes sure there aren't any parts in the feeder initially.
 		assertEquals(feeder.requestedParts.size(),0);
 		assertEquals(feeder.feederSlot,0);
 		System.out.println("feeder name = " + feeder.getName());
 
-		assertEquals(feeder.getName(),"Feeder1");
+		assertEquals(feeder.getName(),"Feeder0");
 		
 	}
 	
 	/** 
 	 * This test makes sure that the Feeder's lanes are getting set up properly.
-	 
+	 **/
+	
 	public void testSetUpLanesAndGantry() {
 		// These lanes were set up in the preconditions test
 		assertEquals(feeder.topLane.lane.getName(),"top");
@@ -67,8 +73,7 @@ public class FeederTests extends TestCase{
 	}
 	
 	
-	
-	/** Test the MESSAGES 
+	/** Test the MESSAGES  **/
 	
 	public void testMsgEmptyNest(){
 		// Some initial setup
@@ -161,15 +166,15 @@ public class FeederTests extends TestCase{
     	assertEquals(feeder.state,FeederAgent.FeederState.WAITING_FOR_PARTS);
 		
     	// Parts Request State after scheduler
-    			for(FeederAgent.MyPartRequest pr : feeder.requestedParts)
-    			{
-    				if (pr.pt == p1)
-    				{
-    					mpr = pr;
-    					assertEquals(pr.state,FeederAgent.MyPartRequestState.ASKED_GANTRY);
-    				}
-    			}
-    	
+		for(FeederAgent.MyPartRequest pr : feeder.requestedParts)
+		{
+			if (pr.pt == p1)
+			{
+				mpr = pr;
+				assertEquals(pr.state,FeederAgent.MyPartRequestState.ASKED_GANTRY);
+			}
+		}
+
     			
     			
 
@@ -206,10 +211,14 @@ public class FeederTests extends TestCase{
 		
 		
 		
+		
+		
+		
+		
 		// SCENARIO #3: The feeder is ready to purge now
 		// some initial setup:
 		feeder.state = FeederAgent.FeederState.OK_TO_PURGE;
-		mpr.state = FeederAgent.MyPartRequestState.NEEDED;
+		//mpr.state = FeederAgent.MyPartRequestState.NEEDED; // I don't think this is necessary.
 		feeder.topLane.part = mpr.pt;
 		
 		
@@ -227,7 +236,6 @@ public class FeederTests extends TestCase{
     	
     	// and check the lane's state
     	assertEquals(feeder.topLane.state,FeederAgent.MyLaneState.PURGING);
-		
 		
 	}
 	
@@ -307,8 +315,236 @@ public class FeederTests extends TestCase{
 	}
 	
 	
+	/** This test tests the following scenario:
+	 * 1) Parts of type A are loaded into the top lane. 
+	 * 2) Parts of type B are loaded into the bottom lane.
+	 * 3) Parts of type A are loaded into the top lane.
+	 *     - Neither lane should purge, only the feeder.
+	 *     - The lane diverter should switch and feed the Parts A into the top lane.
+	 */
+	
+	public void testLaneSwitchCase1() {
+		// some initial setup
+//		feeder.state = FeederAgent.FeederState.WAITING_FOR_PARTS;
+		feeder.msgLaneNeedsPart(p1, top);
+		feeder.pickAndExecuteAnAction();
+		assertEquals(feeder.diverter,DiverterState.FEEDING_BOTTOM); // initially the diverter is feeding the bottom lane
+
+		
+		// #################################################
+		// (1) Parts of type A are loaded into the top lane.
+		// #################################################
+
+		feeder.msgHereAreParts(p1);
+		
+		// The gantry has DELIVERED the part
+		for(FeederAgent.MyPartRequest pr : feeder.requestedParts)
+		{
+			if (pr.pt == p1)
+			{
+				mpr = pr; // set a reference to the MyPartRequest
+				assertEquals(pr.state,FeederAgent.MyPartRequestState.DELIVERED);
+			}
+		}
+		
+		// make sure the scheduler works
+		feeder.pickAndExecuteAnAction();
+		
+		// should remove the request from the list
+		assertFalse(feeder.requestedParts.contains(mpr));
+		
+		// the current part that is being fed into the lane
+		assertEquals(feeder.currentPart,mpr.pt);
+		
+		if (feeder.bottomLane.lane == mpr.lane){
+			assertEquals(feeder.bottomLane.part,mpr.pt);
+		}
+		else {
+			assertEquals(feeder.topLane.part,mpr.pt);
+		}
+		
+		// make sure the feeder's state is updating correctly
+		assertEquals(feeder.state,FeederAgent.FeederState.SHOULD_START_FEEDING);
+		
+		
+		// The feeder receives the parts he asked for from the gantry.
+		feeder.pickAndExecuteAnAction();
+		
+		// diverter was feeding the bottom lane initially, see if it switched correctly
+		assertEquals(feeder.diverter,DiverterState.FEEDING_TOP); 
+		
+			
+		// After the okayToPurgeTimer goes off, the feeder receives a request for new parts.
+		feeder.state = FeederState.OK_TO_PURGE; // this gets called when the okayToPurgeTimer goes off
+		
+		
+		
+		
+		
+		
+		// ####################################################
+		// (2) Parts of type B are loaded into the bottom lane.
+		// ####################################################
+		
+		feeder.msgLaneNeedsPart(p2, bottom);
+
+		feeder.pickAndExecuteAnAction();
+
+		// make sure the feeder's state is updating correctly
+		assertEquals(feeder.state,FeederAgent.FeederState.WAITING_FOR_PARTS);
+
+
+//		assertEquals(feeder.bottomLane.part,p2);
+
+		// Figure out whats going on inside the purge if necessary method
+//		assertTrue(feeder.bottomLane.lane == bottom);
+//		assertTrue(feeder.bottomLane.part != feeder.currentPart);
+
+		
+		
+		feeder.msgHereAreParts(p2);
+
+		// The gantry has DELIVERED the part
+		for(FeederAgent.MyPartRequest pr : feeder.requestedParts)
+		{
+			if (pr.pt == p2)
+			{
+				mpr = pr; // set a reference to the MyPartRequest
+				assertEquals(pr.state,FeederAgent.MyPartRequestState.DELIVERED);
+			}
+		}
+
+		// make sure the scheduler works
+		feeder.pickAndExecuteAnAction();
+
+		// should remove the request from the list
+		assertFalse(feeder.requestedParts.contains(mpr));
+
+		// the current part that is being fed into the lane
+		assertEquals(feeder.currentPart,mpr.pt);
+
+		if (feeder.bottomLane.lane == mpr.lane){
+			assertEquals(feeder.bottomLane.part,mpr.pt);
+		}
+		else {
+			assertEquals(feeder.topLane.part,mpr.pt);
+		}
+
+		
+		// make sure the feeder's state is updating correctly
+		assertEquals(feeder.state,FeederAgent.FeederState.SHOULD_START_FEEDING);
+
+
+		// The feeder receives the parts he asked for from the gantry.
+		feeder.pickAndExecuteAnAction();
+		
+		// the feeder should want to feed the bottomlane
+		assertEquals(feeder.currentPart,feeder.bottomLane.part);
+		
+		// Feed p2 to the bottom lane 
+		assertEquals(feeder.diverter,DiverterState.FEEDING_BOTTOM); 
+
+
+		// Feeder is deciding if it should purge or not
+		assertTrue("Feeder should call the Action purgeIfNecessary(). Event log: "
+    			+ feeder.log.toString(), 
+    			feeder.log.containsString("Action purgeIfNecessary()"));
+
+		// Feeder has decided to purge
+		assertTrue("Feeder should call the Animation DoPurgeFeeder(). Event log: "
+    			+ feeder.log.toString(), 
+    			feeder.log.containsString("Animation DoPurgeFeeder()"));
+
+		// Feeder shouldn't purge either of its lanes
+		assertFalse("Feeder shouldn't call the Animation DoPurgeTopLane(). Event log: "
+				+ feeder.log.toString(), 
+				feeder.log.containsString("Animation DoPurgeTopLane()"));
+		assertFalse("Feeder shouldn't call the Animation DoPurgeBottomLane(). Event log: "
+				+ feeder.log.toString(), 
+				feeder.log.containsString("Animation DoPurgeBottomLane()"));
+		
+		// After the okayToPurgeTimer goes off, the feeder receives a request for new parts.
+		feeder.state = FeederState.OK_TO_PURGE; // this gets called when the okayToPurgeTimer goes off
+		
+
+		
+		
+		// #################################################
+		// (3) Parts of type A are loaded into the top lane.
+		// #################################################
+		feeder.msgLaneNeedsPart(p1, top);
+
+
+		feeder.pickAndExecuteAnAction();
+		
+		// Figure out whats going on inside the askGantyForPart method
+		assertFalse(feeder.currentPart == p1);
+		assertFalse(feeder.state == FeederState.IS_FEEDING);
+		
+		
+		assertEquals(feeder.topLane.part,p1);
+
+		// Figure out whats going on inside the purge if necessary method
+		assertTrue(feeder.topLane.lane == top);
+		assertTrue(feeder.topLane.part != feeder.currentPart); // WHERE IS CURRENTPART GETTING SET?
+		
+		//assertEquals(feeder.state,FeederState.OK_TO_PURGE);  // NOTE: We don't check this because it gets reset during the agent's thread.
+		
+		
+		
+		// Feeder shouldn't purge either of its lanes
+		assertFalse("Feeder shouldn't call the Animation DoPurgeTopLane(). Event log: "
+				+ feeder.log.toString(), 
+				feeder.log.containsString("Animation DoPurgeTopLane()"));
+		assertFalse("Feeder shouldn't call the Animation DoPurgeBottomLane(). Event log: "
+				+ feeder.log.toString(), 
+				feeder.log.containsString("Animation DoPurgeBottomLane()"));
+
+		// make sure the feeder's state is updating correctly
+		assertEquals(feeder.state,FeederAgent.FeederState.WAITING_FOR_PARTS);
+
+		feeder.msgHereAreParts(p1);
+		
+		feeder.log.clear();
+		
+		feeder.pickAndExecuteAnAction(); // puts the feeder in the state SHOULD_START_FEEDING
+		assertEquals(feeder.state,FeederState.SHOULD_START_FEEDING); 
+		
+		
+		feeder.pickAndExecuteAnAction(); // feeder should call the StartFeeding() action
+
+		
+		assertEquals(feeder.topLane.part,feeder.currentPart);
+		
+		//assertEquals(feeder.state,FeederState.IS_FEEDING); 
+
+		
+		// the PREVIOUS lane that was being fed
+		assertEquals(feeder.diverter,DiverterState.FEEDING_BOTTOM); 
+
+		// see if the parts are matching up with the appropriate lane
+		assertEquals(feeder.currentPart,feeder.topLane.part);
+
+		// the top lane should contain parts
+		assertEquals(feeder.topLane.state,MyLaneState.CONTAINS_PARTS);
+		
+		
+		assertTrue("Feeder should be doing Action ProcessFeederParts(). Event log: "
+    			+ feeder.log.toString(), 
+    			feeder.log.containsString("Action ProcessFeederParts()"));
+		
+		// MAKE SURE THE DIVERTER IS SWITCHING CORRECTLY
+		assertTrue("Feeder should be doing Animation DoSwitchLane(). Event log: "
+    			+ feeder.log.toString(), 
+    			feeder.log.containsString("Animation DoSwitchLane()"));
+	
+		
+	}
+	
+	
+	
 }
-*/
+
 
 
 
