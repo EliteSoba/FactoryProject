e41075b79c5c7e88bcaaca1e73ac1b5d29a74379
au-pu <austin.puri@gmail.com>
2012-11-14 18:15:29 -0800

    Changed Stand interface so that I can use Stand objects instead of StandAgent.  Basically added a simple setter and getter for setting/getting kits as well as slot states

--- Factory/src/factory/interfaces/Stand.java
+++ Factory/src/factory/interfaces/Stand.java
@@ -1,6 +1,8 @@
 package factory.interfaces;
 
+import factory.Kit;
 import factory.Kit.KitState;
+import factory.StandAgent.MySlotState;
 
 
 public interface Stand {
@@ -10,5 +12,11 @@ public interface Stand {
 	void msgKitRobotNoLongerUsingStand();
 	void msgPartRobotWantsToPlaceParts();
 	void msgKitRobotWantsToExportKit();
+	
+	public boolean setSlotKit(String slot, Kit k);
+	public Kit getSlotKit(String slot);
+	
+	public boolean setSlotState(String slot, MySlotState state);
+	public MySlotState getSlotState(String slot);
 
 }
