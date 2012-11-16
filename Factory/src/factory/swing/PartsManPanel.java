1497b67267edc20fb75f61d25b3ad0e6050b59e6
mfmendiola <mmendiol@usc.edu>
2012-11-16 02:52:00 -0800

    updated PartsManPanel

--- Factory/src/factory/swing/PartsManPanel.java
+++ Factory/src/factory/swing/PartsManPanel.java
@@ -27,7 +27,7 @@ public class PartsManPanel extends JPanel{
 	// Data
 	JTabbedPane tabbedPane;
 	AddPanel addPanel;
-	RemovePanel removePanel;
+	EditPanel EditPanel;
 	ArrayList<String> fileNames;
 	PartsManager partsManager;
 	JScrollPane scrollPane;
@@ -52,12 +52,12 @@ public class PartsManPanel extends JPanel{
 		fileNames.add("nose");
 		fileNames.add("moustache");
 		fileNames.add("ear");
-		
+
 		model = new PartsTableModel();
 		model.addColumn("ID");
 		model.addColumn("Name");
 		table = new JTable(model);
-		
+
 		TableColumn column = null;
 		for (int i = 0; i < 1; i ++){
 			column = table.getColumnModel().getColumn(i);
@@ -70,17 +70,20 @@ public class PartsManPanel extends JPanel{
 
 		scrollPane = new JScrollPane(table);
 		table.setFillsViewportHeight(true);
-		
-		for(Part temp : partsManager.parts.values()){
-			ImageIcon image = new ImageIcon(temp.imagePath);
-			 Object[] row = {temp.id, image};
-			 model.insertRow(model.getRowCount(), row);
+		for(int i = 1; i < partsManager.parts.size(); i++){
+			for(Part temp : partsManager.parts.values()){
+				if(i == temp.id){
+					ImageIcon image = new ImageIcon(temp.imagePath);
+					Object[] row = {temp.id, image};
+					model.insertRow(model.getRowCount(), row);
+				}
+			}
 		}
 		addPanel = new AddPanel();
-		removePanel = new RemovePanel();
+		EditPanel = new EditPanel();
 		tabbedPane = new JTabbedPane();
 		tabbedPane.addTab("addPanel", addPanel);
-		tabbedPane.addTab("removePanel", removePanel);
+		tabbedPane.addTab("EditPanel", EditPanel);
 		currentListPanel = new JPanel();
 
 		this.add(tabbedPane);
@@ -100,11 +103,17 @@ public class PartsManPanel extends JPanel{
 
 		// Data
 		JLabel title;
-		JLabel label1;
-		JLabel label2;
+		JLabel nameLabel;
+		JLabel imageLabel;
+		JLabel idLabel;
+		JLabel descriptionLabel;
+		JLabel nestStabalizationTimeLabel;
 		JLabel previewFrame;
-		JTextField itemName;
+		JTextField name;
 		JComboBox imageSelection;
+		JSpinner nestStabalizationTime;
+		JTextField description;
+		
 		JButton saveItem;
 
 
@@ -117,14 +126,18 @@ public class PartsManPanel extends JPanel{
 
 			title = new JLabel ("Parts Manager");
 			title.setFont(new Font("Serif", Font.BOLD, 16));
-			label1 = new JLabel ("New Item : ");
-			itemName = new JTextField(8);
-			label2 = new JLabel ("Image : ");
+			nameLabel = new JLabel ("New Item : ");
+			name = new JTextField(8);
+			imageLabel = new JLabel ("Image : ");
+			idLabel = new JLabel("ID# : " + partsManager.parts.size());
+			descriptionLabel = new JLabel("Description : ");
+			nestStabalizationTimeLabel = new JLabel ("Nest Stabalization Time : ");
+			nestStabalizationTime = new JSpinner();
+			description = new JTextField("Enter description here");
 			imageSelection = new JComboBox();
 			for(int i = 0; i < fileNames.size(); i++){
 				imageSelection.addItem(fileNames.get(i));
 			}
-
 			imageSelection.addActionListener(this);
 			previewFrame = new JLabel();
 			ImageIcon imagePreview = new ImageIcon("Images/" + fileNames.get(0) + ".png");
@@ -140,31 +153,52 @@ public class PartsManPanel extends JPanel{
 			c.fill = GridBagConstraints.VERTICAL;
 			c.insets = new Insets(0,0,20,0);
 			this.add(title, c);
-			c.fill = GridBagConstraints.VERTICAL;
+			c.fill = GridBagConstraints.HORIZONTAL;
 			c.gridwidth = 1;
 			c.gridx = 0;
 			c.gridy = 1;
-			this.add(label1, c);
-			c.fill = GridBagConstraints.VERTICAL;
+			this.add(nameLabel, c);
+			c.fill = GridBagConstraints.HORIZONTAL;
 			c.gridx = 1;
 			c.gridy = 1;
-			this.add(itemName, c);
-			c.fill = GridBagConstraints.VERTICAL;
+			this.add(name, c);
+			c.fill = GridBagConstraints.HORIZONTAL;
 			c.gridx = 0;
 			c.gridy = 2;
-			this.add(label2, c);
+			this.add(idLabel, c);
+			c.gridx = 0;
+			c.gridy = 3;
+			c.fill = GridBagConstraints.HORIZONTAL;
+			this.add(nestStabalizationTimeLabel, c);
 			c.gridx = 1;
-			c.gridy = 2;
+			c.gridy = 3;
 			c.fill = GridBagConstraints.VERTICAL;
+			this.add(nestStabalizationTime, c);
+			c.gridx = 0;
+			c.gridy = 4;
+			c.fill = GridBagConstraints.HORIZONTAL;
+			this.add(imageLabel, c);
+			c.gridx = 1;
+			c.gridy = 4;
+			c.fill = GridBagConstraints.HORIZONTAL;
 			this.add(imageSelection, c);
-			c.gridx = 0;
-			c.gridy = 3;
-			c.fill = GridBagConstraints.VERTICAL;
-			this.add(saveItem, c);
-			c.gridx = 1;
-			c.gridy = 3;
-			c.fill = GridBagConstraints.VERTICAL;
+			c.gridx = 2;
+			c.gridy = 4;
+			c.fill = GridBagConstraints.HORIZONTAL;
 			this.add(previewFrame, c);
+			c.gridx = 0;
+			c.gridy = 5;
+			c.fill = GridBagConstraints.HORIZONTAL;
+			this.add(descriptionLabel, c);
+			c.gridx = 1;
+			c.gridy = 5;
+			c.fill = GridBagConstraints.HORIZONTAL;
+			this.add(description, c);
+			c.gridx = 0;
+			c.gridy = 6;
+			c.fill = GridBagConstraints.VERTICAL;
+			c.anchor = GridBagConstraints.LINE_START;
+			this.add(saveItem, c);
 
 		}
 
@@ -177,18 +211,19 @@ public class PartsManPanel extends JPanel{
 			if (ae.getSource() == saveItem){
 				boolean nameTaken = false;
 				/*for(int i = 0; i < partsManager.getParts().size(); i++){
-					 if(itemName.getText().equals(partsManager.getParts().get(i)))
+					 if(name.getText().equals(partsManager.getParts().get(i)))
 						 nameTaken = true;
 				 }*/
 				if(nameTaken){
-					itemName.setText("Name taken.");
+					name.setText("Name taken.");
 				}else{
 					System.out.println("I will create a new part and send it to the server.");
 
 					System.out.println("parts size : " + partsManager.parts.size());
-					Part p = new Part(itemName.getText(), 2, "This is a test.","Images/" + (String)imageSelection.getSelectedItem() + ".png", 2);
+					Part p = new Part(name.getText(), partsManager.parts.size(), description.getText(),"Images/" + (String)imageSelection.getSelectedItem() + ".png", (Integer)nestStabalizationTime.getValue());
 					addItem(p);
 					partsManager.sendMessage("add",p);
+					idLabel = new JLabel("ID# : " + partsManager.parts.size());
 				}
 			}else{
 				JComboBox cb = (JComboBox)ae.getSource();
@@ -201,73 +236,118 @@ public class PartsManPanel extends JPanel{
 
 	}
 
-	private class RemovePanel extends JPanel implements ActionListener{
+	private class EditPanel extends JPanel implements ActionListener{
 		// Data
 
-		JLabel title;
-		JLabel label1;
-		JLabel previewFrame;
-		JComboBox imageSelection;
-		JButton removeItem;
+		// Data
+				JLabel title;
+				JLabel nameLabel;
+				JLabel imageLabel;
+				JLabel idLabel;
+				JLabel descriptionLabel;
+				JLabel nestStabalizationTimeLabel;
+				JLabel previewFrame;
+				JTextField name;
+				JComboBox imageSelection;
+				JSpinner nestStabalizationTime;
+				JTextField description;
+				
+				JButton saveItem;
+				JButton removeItem;
+			
 
-		//Methods
 
-		public RemovePanel(){
-			this.setLayout(new GridBagLayout());
-			GridBagConstraints c = new GridBagConstraints();
+				//Methods
+
+				public EditPanel(){
+					this.setLayout(new GridBagLayout());
+					GridBagConstraints c = new GridBagConstraints();
 
 
-			title = new JLabel ("Parts Manager");
-			title.setFont(new Font("Serif", Font.BOLD, 16));
-			label1 = new JLabel ("Item : ");
-			imageSelection = new JComboBox();
-			/*Iterator it = partsManager.getParts().entrySet().iterator();
+					title = new JLabel ("Parts Manager");
+					title.setFont(new Font("Serif", Font.BOLD, 16));
+					nameLabel = new JLabel ("New Item : ");
+					name = new JTextField(8);
+					imageLabel = new JLabel ("Image : ");
+					idLabel = new JLabel("ID# : " + partsManager.parts.size());
+					descriptionLabel = new JLabel("Description : ");
+					nestStabalizationTimeLabel = new JLabel ("Nest Stabalization Time : ");
+					nestStabalizationTime = new JSpinner();
+					description = new JTextField("Enter description here");
+					imageSelection = new JComboBox();
+					for(int i = 0; i < fileNames.size(); i++){
+						imageSelection.addItem(fileNames.get(i));
+					}
+					imageSelection.addActionListener(this);
+					previewFrame = new JLabel();
+					ImageIcon imagePreview = new ImageIcon("Images/" + fileNames.get(0) + ".png");
+					previewFrame.setIcon(imagePreview);
+					saveItem = new JButton ("Save Changes");
+					saveItem.addActionListener(this);
+					removeItem = new JButton ("Remove Item");
+					removeItem.addActionListener(this);
 
-					while(it.hasNext()){
-						Map.Entry keyPair = (Map.Entry)it.next();
-						imageSelection.addItem(keyPair.getValue().imagePath);
-						it.remove();
-					}*/
-			for ( Part p : partsManager.getParts().values()){
-				imageSelection.addItem(p.name);
-				System.out.println(p.name);
-			}
-			imageSelection.addActionListener(this);
-			previewFrame = new JLabel();
-			System.out.println("Hey " + (String)imageSelection.getItemAt(0));
-			//ImageIcon imagePreview = new ImageIcon((String)imageSelection.getItemAt(0));
-			//previewFrame.setIcon(imagePreview);
+					//currentList = new JScrollPane();
 
-			removeItem = new JButton ("Remove Item");
-			removeItem.addActionListener(this);
+					c.gridx = 0;
+					c.gridy = 0;
+					c.gridwidth = 2;
+					c.fill = GridBagConstraints.VERTICAL;
+					c.insets = new Insets(0,0,20,0);
+					this.add(title, c);
+					c.fill = GridBagConstraints.HORIZONTAL;
+					c.gridwidth = 1;
+					c.gridx = 0;
+					c.gridy = 1;
+					this.add(nameLabel, c);
+					c.fill = GridBagConstraints.HORIZONTAL;
+					c.gridx = 1;
+					c.gridy = 1;
+					this.add(name, c);
+					c.fill = GridBagConstraints.HORIZONTAL;
+					c.gridx = 0;
+					c.gridy = 2;
+					this.add(idLabel, c);
+					c.gridx = 0;
+					c.gridy = 3;
+					c.fill = GridBagConstraints.HORIZONTAL;
+					this.add(nestStabalizationTimeLabel, c);
+					c.gridx = 1;
+					c.gridy = 3;
+					c.fill = GridBagConstraints.VERTICAL;
+					this.add(nestStabalizationTime, c);
+					c.gridx = 0;
+					c.gridy = 4;
+					c.fill = GridBagConstraints.HORIZONTAL;
+					this.add(imageLabel, c);
+					c.gridx = 1;
+					c.gridy = 4;
+					c.fill = GridBagConstraints.HORIZONTAL;
+					this.add(imageSelection, c);
+					c.gridx = 2;
+					c.gridy = 4;
+					c.fill = GridBagConstraints.HORIZONTAL;
+					this.add(previewFrame, c);
+					c.gridx = 0;
+					c.gridy = 5;
+					c.fill = GridBagConstraints.HORIZONTAL;
+					this.add(descriptionLabel, c);
+					c.gridx = 1;
+					c.gridy = 5;
+					c.fill = GridBagConstraints.HORIZONTAL;
+					this.add(description, c);
+					c.gridx = 0;
+					c.gridy = 6;
+					c.fill = GridBagConstraints.VERTICAL;
+					c.anchor = GridBagConstraints.LINE_START;
+					this.add(saveItem, c);
+					c.gridx = 1;
+					c.gridy = 6;
+					c.fill = GridBagConstraints.VERTICAL;
+					c.anchor = GridBagConstraints.LINE_START;
+					this.add(removeItem, c);
+					
 
-			c.gridx = 0;
-			c.gridy = 0;
-			c.gridwidth = 2;
-			c.fill = GridBagConstraints.VERTICAL;
-			c.insets = new Insets(0,0,20,0);
-			this.add(title, c);
-			c.fill = GridBagConstraints.VERTICAL;
-			c.fill = GridBagConstraints.HORIZONTAL;
-			c.gridwidth = 1;
-			c.gridx = 0;
-			c.gridy = 1;
-			this.add(label1, c);
-			c.fill = GridBagConstraints.VERTICAL;
-			c.fill = GridBagConstraints.HORIZONTAL;
-			c.gridx = 1;
-			c.gridy = 1;
-			this.add(imageSelection, c);
-			c.fill = GridBagConstraints.VERTICAL;
-			c.fill = GridBagConstraints.HORIZONTAL;
-			c.gridx = 2;
-			c.gridy = 1;
-			this.add(previewFrame, c);
-			c.gridx = 0;
-			c.gridy = 2;
-			c.fill = GridBagConstraints.VERTICAL;
-			c.fill = GridBagConstraints.HORIZONTAL;
-			this.add(removeItem, c);
 
 		}
 
@@ -282,8 +362,6 @@ public class PartsManPanel extends JPanel{
 				System.out.println("I will remove a part and update the server.");
 				partsManager.sendMessage("remove", partsManager.parts.get((String)imageSelection.getSelectedItem()));
 				removeItem(partsManager.parts.get((String)imageSelection.getSelectedItem()));
-				partsManager.parts.remove((String)imageSelection.getSelectedItem());
-				
 			}else{
 				JComboBox cb = (JComboBox)ae.getSource();
 				String petName = (String)cb.getSelectedItem();
@@ -294,7 +372,7 @@ public class PartsManPanel extends JPanel{
 
 	}
 
-	
+
 	public void addItem(Part p){
 		partsManager.parts.put(p.name, p);
 		ImageIcon image = new ImageIcon(p.imagePath);
@@ -303,22 +381,21 @@ public class PartsManPanel extends JPanel{
 		table.revalidate();
 	}
 	public void removeItem(Part p){
-		
-		for(String name : partsManager.parts.keySet()){
-			System.out.println("Part" + p.name);
-		}
+		System.out.println("Removing " + p.name + " " + p.id);
 		for(int i = 0; i < model.getRowCount(); i++){
-			if((Integer)p.id == (Integer)model.getValueAt(i, 0));
-			model.removeRow(i);
-			break;
-			
+			System.out.println("Row : " + (Integer)model.getValueAt(i, 0));
+			if((Integer)p.id == (Integer)model.getValueAt(i, 0)){
+				model.removeRow(i);
+				break;
+			}
+
 		}
 		partsManager.parts.remove(p.name);
-		
+
 		table.revalidate();
 	}
 
-	
+
 
 	public class PartsTableModel extends DefaultTableModel{
 		public boolean isCellEditable(int row, int column){
