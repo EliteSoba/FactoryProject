3a19f1c45d117e4e060f291bb7a1a666e75b8a32
Ryan Cleary <rcleary@usc.edu>
2012-11-17 22:34:18 -0800

    among other things, made a small change to the Vision Interface.

--- Factory/src/factory/interfaces/Vision.java
+++ Factory/src/factory/interfaces/Vision.java
@@ -4,6 +4,6 @@ import java.util.TimerTask;
 import factory.Kit;
 public interface Vision {
 
-	public void msgMyNestsReadyForPicture(Nest nest, Nest nest2, TimerTask timerTask);
+	public void msgMyNestsReadyForPicture(Nest nest, Nest nest2, Feeder feeder);
 	public void msgAnalyzeKitAtInspection(Kit kit);
 }
