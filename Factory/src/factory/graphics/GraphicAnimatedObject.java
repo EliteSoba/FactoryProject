d5f47b54b70920928a5a4455d62e7dc551731a78
George Li <ligeorge@usc.edu>
2012-11-17 22:26:38 -0800

    Restructured inheritance for robots, animatedObjects, and nests.

--- Factory/src/factory/graphics/GraphicAnimatedObject.java
+++ Factory/src/factory/graphics/GraphicAnimatedObject.java
@@ -11,25 +11,11 @@ class GraphicAnimatedObject
 	protected int dtheta;						// change in image angle
 	protected int imageWidth, imageHeight;		// image size
 	protected Image image;
-	protected ArrayList<GraphicItem> items;			// inventory of items
 	
 	// Constructors
 	public GraphicAnimatedObject()
 	{
 		
-	}
-	public GraphicAnimatedObject(int init_x, int init_y, int init_theta, int init_dx, int init_dy, int init_dtheta, int init_imageWidth, int init_imageHeight, String init_imagePath)
-	{
-		items = new ArrayList<GraphicItem>();
-		x = init_x;
-		y = init_y;
-		theta = init_theta;
-		dx = init_dx;
-		dy = init_dy;
-		dtheta = init_dtheta;
-		imageWidth = init_imageWidth;
-		imageHeight = init_imageHeight;
-		image = Toolkit.getDefaultToolkit().getImage(init_imagePath);
 	}
 	
 	// Set functions
@@ -76,55 +62,5 @@ class GraphicAnimatedObject
 	public int getImageHeight()
 	{
 		return imageHeight;
-	}
-	public void addItem(GraphicItem newItem)
-	{
-		items.add(newItem);
-	}
-	public void clearItems()
-	{
-		items.clear();
-	}
-	public boolean hasItem()
-	{
-		if(items.size() >= 1)
-			return true;
-		else
-			return false;
-	}
-	public GraphicItem popItem()
-	{
-		if (items.size() == 0)
-			return null;
-		GraphicItem lastItem = items.get(items.size()-1);		// get last item
-		items.remove(items.size()-1);					// remove last item
-		return lastItem;								// return last item
-	}
-	/**
-	 * Gets an Item at the provided index and removes the Item
-	 * @param index The index of the Item being taken
-	 * @return The Item at the provided index; returns {@code null} if the index is invalid
-	 */
-	public GraphicItem popItemAt(int index) {
-		if (index < 0 || index >= items.size())
-			return null;
-		GraphicItem returnedItem = items.get(index);
-		items.remove(index);
-		return returnedItem;
-	}
-	public int getSize()
-	{
-		return items.size();
-	}
-	public GraphicItem getItemAt(int i)
-	{
-		return items.get(i);
-	}
-	// Update functions
-	public void move()
-	{
-		x += dx;
-		y += dy;
-		theta += dtheta;
 	}
 }
