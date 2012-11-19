77b1a146e35289f65b3cfacf374b3ea3bb7a7099
Ben Mayeux <benmayeux@gmail.com>
2012-11-18 20:26:32 -0800

    added close operation for relay with server to fmp

--- Factory/src/factory/client/Client.java
+++ Factory/src/factory/client/Client.java
@@ -2,6 +2,8 @@ package factory.client;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -19,7 +21,7 @@ import javax.swing.Timer;
 
 /*Abstract class used by each client
  * written by Ben Mayeux
- * 
+ * aaaa
  */
 
 
@@ -27,9 +29,7 @@ import javax.swing.Timer;
 public abstract class Client extends JFrame implements ActionListener {
 	
 	public enum Type{//enumeration for the 6 types
-		KITMANAGER, PARTSMANAGER, FACTORYPRODUCTIONMANAGER,
-		KITASSEMBLYMANAGER, LANEMANAGER,
-		GANTRYROBOTMANAGER
+		 fpm, gm, kam, km, lm, pm;
 	}
 	protected Socket server; //connection to server
 	protected JPanel graphics; //possibly null graphics frame (graphics team)
@@ -39,62 +39,153 @@ public abstract class Client extends JFrame implements ActionListener {
 	public BufferedReader input; //input stream from server
 	public String currentCommand; //current command string from server
 	public ArrayList<String> parsedCommand; //current command parsed into strings
-	protected Timer updater; //repaints graphics
+	// Timer updater; //repaints graphics
 	boolean connected;
 	
 	
 	Runnable independentInput = new Runnable(){
 		public void run(){
-			for(;;){
+			while(true){
+//				System.out.println("1");
 				if(connected)
 				try {
-					input.readLine(); //reads from input each time there is a new string
+					currentCommand = input.readLine(); //reads from input each time there is a new string
 					parseInput();
+					doCommand(parsedCommand);
 				} catch (Exception e) {
-					System.out.println("inputStream not open");
+					//System.out.println("inputStream not open"); // not necessary 
 				}
 			}//end for
 		}//end run
 	}; //end independentInput
 	
 	public Client(Type t, JPanel buttons, JPanel Animation){
+		
 		connected = false;
 		type = t;
 		UI = buttons;
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.graphics = Animation;
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		graphics = Animation;
 		connect(); //connects to server
-		updater = new Timer(1000/30, this); //sets timer to update graphics
-		setInterface(); //to be implemented...set size, layout, add UI etc...
+		System.out.println(connected);
+		//updater = new Timer(1000/30, this); //sets timer to update graphics
+		//updater.start();
+		//setInterface(); //to be implemented...set size, layout, add UI etc...
 		Thread inputThread = new Thread(independentInput);
-		inputThread.run();
+		inputThread.start();
+		this.addWindowListener(new WindowCloser(this));
+		
+		
 	}
-	
+		
 	public void connect(){
 		try {
 			server = new Socket("127.0.0.1", 12321); //connects to server on localhost
 			input = new BufferedReader(new InputStreamReader(server.getInputStream()));//opens inputStream
-			output = new PrintWriter(new OutputStreamWriter(server.getOutputStream()));//opens outputStream
-			connected = true;
+			output = new PrintWriter(server.getOutputStream(),true);//opens outputStream
+			System.out.println(this.type.toString());
+			output.println(this.type.toString());
+			
+			String reply = input.readLine();
+			System.out.println(reply);
+			if(reply.equals("connected")){
+				connected = true;
+			}
 			System.out.println("connected to server!");
+			System.out.println("waiting for agents to start...");
+			input.readLine();
+			System.out.println("Factory Started....yo!");
 		} catch (Exception e) {
 			System.out.println("Host unavailable");
-		} 
+		} 	
 	}
 	
 	public void parseInput(){
-		//TODO parseinput
-		parsedCommand = new ArrayList<String>(Arrays.asList(currentCommand.split(" "))); //puts string into array list
-	
+		parsedCommand = new ArrayList<String>(Arrays.asList(currentCommand.split(" "))); //puts string into array list   
+    }
+	public void sendCommand(String cmd){
+		output.println(cmd);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		if(arg0.getSource() == updater)
+		/*if(arg0.getSource() == updater)
 			if(graphics != null)
-				graphics.repaint();
+				graphics.repaint();*/
+		
 	}
 	
-	public abstract void setInterface();//to be implemented by child class	
+
+	public abstract void setInterface(); // to be implemented by child class
+    public abstract void doCommand(ArrayList<String> pCmd); // also must be implemented by child class
+    
+    private class WindowCloser implements WindowListener{
+
+    	Client client;
+    	public WindowCloser(Client client2) {
+			client = client2;
+		}
+		
+		@Override
+		public void windowActivated(WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowClosed(WindowEvent e) {
+			
+		}
+
+		@Override
+		public void windowClosing(WindowEvent e) {
+			try {
+				if(client.type == Type.fpm)
+					client.sendCommand("fpm mcs cmd stopfactory");
+				client.dispose();
+				System.out.println("resources released");
+				if(client.input != null)
+				{	
+					System.out.println("input stream closing...");
+					client.input.close();
+					System.out.println("input closed");
+				}
+				if(client.output != null)
+					System.out.println("output stream closing...");
+					client.output.close();
+					System.out.println("output closed");
+				} catch (IOException e1) {
+					//never executes but wont compile w/o
+				}
+			System.out.println("window closed");
+			this.client.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		}
+
+		@Override
+		public void windowDeactivated(WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowDeiconified(WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowIconified(WindowEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void windowOpened(WindowEvent e) {
+			// TODO Auto-generated method stub
+			System.out.println("Window opened");
+		}
+    }
+    
+    
 }
 
