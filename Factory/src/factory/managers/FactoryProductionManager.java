7a9b548700b1f4745d7048f52c161e3f40673253
Joey Huang <joeyhuan@usc.edu>
2012-11-18 22:00:29 -0800

    corrected add part message

--- Factory/src/factory/managers/FactoryProductionManager.java
+++ Factory/src/factory/managers/FactoryProductionManager.java
@@ -226,7 +226,7 @@ public class FactoryProductionManager extends Client {
 				((FactoryProdManPanel) UI).removeKit(pCmd.get(2));
 			}
 			else if (identifier.equals("addpartname")) {	// add new part to parts list
-				Part part = new Part(pCmd.get(3),Integer.parseInt(pCmd.get(4)),pCmd.get(7),pCmd.get(5),Integer.parseInt(pCmd.get(6)));
+				Part part = new Part(pCmd.get(2),Integer.parseInt(pCmd.get(3)),pCmd.get(6),pCmd.get(4),Integer.parseInt(pCmd.get(5)));
 				partsList.put(pCmd.get(3),part);
 			}
 			else if (identifier.equals("rmpartname")) {		// remove part from parts list
