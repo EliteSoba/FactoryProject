c2c81bd890fa5236b2cd19482e4dddebd2c39212
George Li <ligeorge@usc.edu>
2012-11-27 01:03:18 -0800

    Updated Toby's conveyor belt (temporarily commented out, will be implemented later today) Added a "static class" for loading image sets from folders Added conveyor belt image set

---
+++ Factory/src/factory/graphics/GraphicAnimation.java
@@ -0,0 +1,27 @@
+package factory.graphics;
+
+import java.awt.Image;
+import java.io.File;
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+
+public enum GraphicAnimation
+{;
+	public static ArrayList<Image> loadAnimationFromFolder(String folderPath, int startingIndex, String fileExtension)
+	{
+		ArrayList<Image> imageList = new ArrayList<Image>();
+		int i = startingIndex;
+		while(true)
+		{
+			String filePath = folderPath + i + fileExtension;
+			File f = new File(filePath);
+			//System.out.println(filePath);
+			if(f.exists())
+				imageList.add(new ImageIcon(filePath).getImage());
+			else
+				break;
+			i++;
+		}
+		return imageList;
+	}
+}
