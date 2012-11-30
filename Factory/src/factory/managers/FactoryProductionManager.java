db476ea9d517914ec5da48183c956f86410ac7fc
Joey Huang <joeyhuan@usc.edu>
2012-11-29 18:30:15 -0800

    fpm messages nonnorm pt2

--- Factory/src/factory/managers/FactoryProductionManager.java
+++ Factory/src/factory/managers/FactoryProductionManager.java
@@ -279,6 +279,19 @@ public class FactoryProductionManager extends Client {
 			
 				System.out.println(partsList.get(pCmd.get(2)));
 			}
+			else if (identifier.equals("nonnormative")) {
+				String caseName = pCmd.get(2);
+				if (caseName.equals("lanejam")) {
+					int lanenum = Integer.parseInt(pCmd.get(3));
+					lanenum = lanenum+1;
+					 ((FactoryProdManPanel)UI).addMessage("A lane jam has occurred in lane " + lanenum + ".");
+				}
+				else if (caseName.equals("slowdiverter")) {
+					int feedernum = Integer.parseInt(pCmd.get(3));
+					feedernum = feedernum+1;
+					((FactoryProdManPanel)UI).addMessage("The diverter at feeder " + feedernum + "switched over late.");
+				}
+			}
 		}
 
 
