2d2214391a3f1e11944d925019fb3662c5bc7468
minhla <minht@usc.edu>
2012-11-30 06:24:38 -0800

    re: mybad. it's 6am. excuse my commits

--- Factory/src/factory/graphics/GraphicLaneManager.java
+++ Factory/src/factory/graphics/GraphicLaneManager.java
@@ -1038,30 +1038,7 @@ public class GraphicLaneManager{
 	public void changeBottomLaneAmplitude(int amp){
 		vibrationAmplitude = amp;	//change to bottom vibrationAmp
 	}
-	
-	public void increaseTopLaneAmplitude(){
-		vibrationAmplitude++;
-		if(vibrationAmplitude > 8)
-			vibrationAmplitude = 8;
-	}
-	
-	public void decreaseTopLaneAmplitude(){
-		vibrationAmplitude--;
-		if(vibrationAmplitude < 1)
-			vibrationAmplitude = 1;
-	}
-	
-	public void increaseBottomLaneAmplitude(){
-		vibrationAmplitude++;		//change to bottom vibrationAmplitude
-		if(vibrationAmplitude > 8)
-			vibrationAmplitude = 8;
-	}
-	
-	public void decreaseBottomLaneAmplitude(){
-		vibrationAmplitude--;		//change to bottom vibrationAmplitude
-		if(vibrationAmplitude < 1)
-			vibrationAmplitude = 1;
-	}
+
 	
 }
 
