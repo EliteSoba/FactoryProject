3019ae1a990ee2a0991beed76bb856b8b8fa0f76
EliteSoba <tobiasle@usc.edu>
2012-12-02 15:52:31 -0800

    SORRY I MESSED UP

--- Factory/src/factory/graphics/GraphicPanel.java
+++ Factory/src/factory/graphics/GraphicPanel.java
@@ -511,22 +511,24 @@ public abstract class GraphicPanel extends JPanel implements ActionListener{
 	/**
 	 * Starts up the designated Feeder
 	 * @param feederNum The designated Feeder
+	 * @see #startFeederDone(int)
 	 */
 	public void turnFeederOn(int feederNum){
 		if (isLaneManager || isFactoryProductionManager) {
 			lane[feederNum].feederOn = true;
-			//startFeederDone(feederNum);
+			startFeederDone(feederNum);
 		}
 	}
 	
 	/**
 	 * Turns off the designated Feeder
 	 * @param feederNum The designated Feeder
+	 * @see #stopFeederDone(int)
 	 */
 	public void turnFeederOff(int feederNum){
 		if (isLaneManager || isFactoryProductionManager) {
 			lane[feederNum].feederOn = false;
-			//stopFeederDone(feederNum);
+			stopFeederDone(feederNum);
 		}
 	}
 	
@@ -973,13 +975,13 @@ public abstract class GraphicPanel extends JPanel implements ActionListener{
 		sendMessage("fa cnf " + feederNum);
 	}
 	
-	/*public void startFeederDone(int feederNum) {
+	public void startFeederDone(int feederNum) {
 		sendMessage("fa cnf " + feederNum);
 	}
 	
 	public void stopFeederDone(int feederNum) {
 		sendMessage("fa cnf " + feederNum);
-	}*/
+	}
 	
 	public void switchFeederLaneDone(int feederNum) {
 		sendMessage("fa cnf " + feederNum);
